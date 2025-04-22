a194bc2ae709ede379cf643b704e24c8d4317efb
ballaboidi <mario@autherith-media.eu>
2025-04-22 09:54:37 +0200

    add tailwindcss to lang servers

--- config/nvim/lua/custom/init.lua
+++ config/nvim/lua/custom/init.lua
@@ -2,5 +2,6 @@ return {
   nvimtree = require("custom.nvim-tree"),
   mappings = require("custom.mappings"),
   options = require("custom.options"),
+  tailwindcss = require("custom.tailwindcss"),
   -- other configs here
 }
