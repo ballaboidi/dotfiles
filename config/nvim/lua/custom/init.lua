eceae84c24bc66c5f978590dce8865fdebf16801
ballaboidi <mario@autherith-media.eu>
2025-04-23 09:51:02 +0200

    move all files

--- config/nvim/lua/custom/init.lua
+++
@@ -1,7 +0,0 @@
-return {
-  nvimtree = require("custom.nvim-tree"),
-  mappings = require("custom.mappings"),
-  options = require("custom.options"),
-  tailwindcss = require("custom.tailwindcss"),
-  -- other configs here
-}
---
+++ nvim/lua/custom/init.lua
@@ -0,0 +1,6 @@
+return {
+  nvimtree = require("custom.nvim-tree"),
+  mappings = require("custom.mappings"),
+  options = require("custom.options"),
+  -- other configs here
+}
