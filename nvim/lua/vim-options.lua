ad0820a54c9dd959c95df6e4d7b17d9c31729283
ballaboidi <mario@autherith-media.eu>
2025-04-24 08:54:13 +0200

    config update

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -2,8 +2,26 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
 vim.opt.termguicolors = true
 vim.g.mapleader = " "
 vim.g.user_emmet_leader_key="<C-e>"
 vim.opt.number = true          -- Show absolute line number
 vim.opt.relativenumber = true -- Show relative line numbers
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+-- Add Terminal commands within neovim
+-- Open terminal in horizontal split
+vim.keymap.set("n", "<leader>h", function()
+  vim.cmd("split | terminal")
+end, { desc = "Open horizontal terminal" })
+
+-- Open terminal in vertical split
+vim.keymap.set("n", "<leader>v", function()
+  vim.cmd("vsplit | terminal")
+end, { desc = "Open vertical terminal" })
