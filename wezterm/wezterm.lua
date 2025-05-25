a9fdfe7abfc81cf3170de548a3f946e3d4e3751e
ballaboidi <mario@autherith-media.eu>
2025-05-25 11:26:51 +0200

    finalize wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,14 +10,26 @@ config.font_size = 14
 config.enable_tab_bar = false
 
 -- add colors to config --
+config.color_scheme = "Nightfox"
 
 config.colors = {
-	background = "#192330",
-	foreground = "#C3C6CA",
+  foreground = "#c9d1d9",
+  background = "#192330",
 }
 
+config.window_decorations = "TITLE"
 
-config.window_background_opacity = 0.9
-config.macos_window_background_blur = 40
+config.window_frame = {
+  -- Title bar colors inspired by Nightfox palette
+  active_titlebar_bg = "#192330",  -- Nightfox bg
+  inactive_titlebar_bg = "#1e293b", -- Slightly dimmer
+  active_titlebar_fg = "#cdcecf",  -- Light gray for text
+  inactive_titlebar_fg = "#a5b4fc", -- Muted blue-violet
+
+  font = wezterm.font({ family="FiraCode Nerd Font", weight="Bold" }),
+  font_size = 14
+}
+-- config.window_background_opacity = 0.9
+-- config.macos_window_background_blur = 10
 
 return config
